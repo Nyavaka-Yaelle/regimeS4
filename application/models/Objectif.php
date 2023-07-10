0906c0617497bde6411e8628feee4a258f4af76c
FitiaLalaina <fitialalainarad@gmail.com>
2023-07-10 14:46:44 +0300

    TypeObjectif Objectif class

--- application/models/Objectif.php
+++ application/models/Objectif.php
@@ -4,12 +4,14 @@
     class Objectif extends CI_Model
     {
         private $idObjectif;
+        private $idTypeObjectif;
         private $nom;
 
         public function insertDonne()
         {
             $data = array(
                 'idObjectif' => $this->getIdObjectif(),
+                'idTypeObjectif' => $this->getIdTypeObjectif(),
                 'nom' => $this->getNom()
             );
             $this->db->insert('Objectif', $data);
@@ -23,6 +25,7 @@
             foreach ($query->result() as $row) {
                 $typeObjectf = new Objectif();
                 $typeObjectf->setIdObjectif($row->idObjectif);
+                $typeObjectf->setIdTypeObjectif($row->idTypeObjectif);
                 $typeObjectf->setNom($row->nom);
                 $results[] = $typeObjectf;
             }
@@ -38,6 +41,12 @@
         }
         public function getIdObjectif(){
             return $this->idObjectif;
+        }
+        public function setIdTypeObjectif($idTypeObjectif){
+            $this->idTypeObjectif = $idTypeObjectif;
+        }
+        public function getIdTypeObjectif(){
+            return $this->idTypeObjectif;
         }
         public function setNom($nom){
             $this->nom = $nom;
