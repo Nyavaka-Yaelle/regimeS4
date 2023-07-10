a510d7ab43584fcf5abab2e0848d76b58369ff97
FitiaLalaina <fitialalainarad@gmail.com>
2023-07-10 14:33:59 +0300

    Correction 2 database

--- database/base.sql
+++ database/base.sql
@@ -8,73 +8,72 @@ create table Utilisateur(
     `motDePasse` varchar(100) not null,
     `identification` int not null
 );
-create table Profiles{
+create table Profiles(
     `idProfiles` int primary key auto_increment,
-    `idUtilisateur` int references Utilisateur(idUtilisateur) unique,
-    `genre` enum('1','0'), --1 homme  --0 femme
+    `idUtilisateur` int references Utilisateur(idUtilisateur),
+    `genre` int not null,
     `taille` double precision not null,
     `poids` double precision not null,
     `dateNaissance` date not null
-};
-create table TypeObjectif{
+);
+create table TypeObjectif(
     `idTypeObjectif` integer primary key auto_increment,
     `nom` varchar(100) not null unique
-};
-create table Objectif{
+);
+create table Objectif(
     `idObjectif` integer primary key auto_increment,
     `idTypeObjectif` integer references TypeObjectif(idTypeObjectif),
     `nom` varchar(100) not null unique
-};
-create table ObjectifUtilisateur{
+);
+create table ObjectifUtilisateur(
     `idObjectifUtilisateur` integer primary key auto_increment,
     `idUtilisateur` integer references Utilisateur(idUtilisateur),
     `idObjectif` integer references Objectif(idObjectif)
-};
-create table TypeSakafo{
+);
+create table TypeSakafo(
     `idTypeSakafo` integer primary key auto_increment,
     `nom` varchar(100) not null
-};
-create table Sakafo{
+);
+create table Sakafo(
     `idSakafo` integer primary key auto_increment,
-    `idTypeSakafo` integer references TypeSakafo(idTypeSakafo)
+    `idTypeSakafo` integer references TypeSakafo(idTypeSakafo),
     `nom` varchar(100) not null,
     `prix` double precision not null
-};
-create table Regime{
+);
+create table Regime(
     `idRegime` integer primary key auto_increment,
     `idUtilisateur` integer references Utilisateur(idUtilisateur),
     `debutRegime` date not null,
     `finRegime` date not null
-};
-create table TypeEnchainement{
+);
+create table TypeEnchainement(
     `idTypeEnchainement` integer primary key auto_increment,
     `nom` varchar(100) unique not null
-};
-create table Enchainement{
+);
+create table Enchainement(
     `idEnchainement` integer primary key auto_increment,
     `idTypeEnchainement` integer references TypeEnchainement(idTypeEnchainement),
     `nom` varchar(100) unique not null,
     `duree` integer not null
-};
---duree minute
-create table Activite{
+);
+create table Activite(
     `idActivite` integer primary key auto_increment,
     `idEnchainement` integer references Enchainement(idEnchainement)
-};
-create table RegimeJournalier{
+);
+create table RegimeJournalier(
     `idRegime` integer primary key auto_increment,
-    `numeroJour` integer,
+    `numeroJour` integer not null,
     `idSakafo` integer references Sakafo(idSakafo),
     `idActivite` integer references Activite(idActivite)
-};
-create table Carte{
+);
+create table Carte(
     `idCarte` integer auto_increment primary key,
-    `code` varchar(14),
+    `code` varchar(14) not null,
     `montant` double precision not null
-};
-create table CarteValider{
+);
+create table CarteValider(
     `idCarteValider` integer auto_increment primary key,
     `idCarte` integer references Carte(idCarte),
-    `dateValidation` date,
+    `dateValidation` date not null,
     `idUtilisateur` integer references Utilisateur(idUtilisateur)
-};
+);
