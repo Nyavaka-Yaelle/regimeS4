4589721fb4782219364c2aeaee7908d4e9cffda9
FitiaLalaina <fitialalainarad@gmail.com>
2023-07-10 09:03:47 +0300

    Css Login Inscription

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://localhost/examenfinals4/';
+$config['base_url'] = 'http://localhost/regimeS4/';
 
 // $autoload['libraries'] = array('database'); // Les librairies
 // $autoload['helper'] = array('url', 'text'); // Les helpers
